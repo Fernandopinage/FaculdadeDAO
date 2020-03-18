e27517dbb01050f18891f18e359e352ee11e21bc
Fernando Pinage <luizfernandoluck@hotmail.com>
2020-03-18 18:33:52 -0300

    ok

--- app/alunoDelete.php
+++ app/alunoDelete.php
@@ -6,5 +6,4 @@ require_once "../app/dao/alunoDao.php";
 $aluno = new AlunoDao();    // instanciando uma classe ClassAluno
 $aluno->deleteAluno(11);    // enviando o ID para função "deleteAluno" do AlunoDao
 
-
 ?>
